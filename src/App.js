20706700fa0ba27d11eb38188ceffec392b7372f
Krishan <kmkrider1999@gmail.com>
2023-07-22 15:40:32 +0530

    Fetched data using unsplash-js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import "./App.css";
 import { useEffect, useReducer, useState } from "react";
+import SearchBar from "./Components/SearchBar";
 // import useHttp from "./custom-hook/useHttp";
 
 const api_url = "https://api.unsplash.com/photos/?client_id=";
 
 const App = () => {
-  const [apiData, setApiData] = useState();
-  const fetchData = async () => {
-    const response = await fetch(
-      api_url + process.env.REACT_APP_UNSPLASH_API_KEY
-    );
-    const data = await response.json();
-
-    setApiData(data);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  console.log(apiData);
-
   return (
     <div>
-      {apiData.map((data) => (
-        <>
-          <img src={data.urls.raw} style={{ height: "10rem" }} />
-        </>
-      ))}
+      <SearchBar />
     </div>
   );
 };
